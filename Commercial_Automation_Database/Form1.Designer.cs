b8ac45872ec78e6f8f4bcd223175ab388af2285f
Furkan Kılıç <furkan.samet888@gmail.com>
2025-07-15 12:51:49 +0300

    Created menu interface, added buttons and edited

---
+++ Commercial_Automation_Database/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Commercial_Automation_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            anasayfaToolStripMenuItem = new ToolStripMenuItem();
+            ürünlerToolStripMenuItem = new ToolStripMenuItem();
+            stoklarToolStripMenuItem = new ToolStripMenuItem();
+            müşterilerToolStripMenuItem = new ToolStripMenuItem();
+            şirketlerToolStripMenuItem = new ToolStripMenuItem();
+            personellerToolStripMenuItem = new ToolStripMenuItem();
+            giderlerToolStripMenuItem = new ToolStripMenuItem();
+            kasaToolStripMenuItem = new ToolStripMenuItem();
+            bankalarToolStripMenuItem = new ToolStripMenuItem();
+            faturalarToolStripMenuItem = new ToolStripMenuItem();
+            yardımToolStripMenuItem = new ToolStripMenuItem();
+            ayarlarToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { anasayfaToolStripMenuItem, ürünlerToolStripMenuItem, stoklarToolStripMenuItem, müşterilerToolStripMenuItem, şirketlerToolStripMenuItem, personellerToolStripMenuItem, giderlerToolStripMenuItem, kasaToolStripMenuItem, bankalarToolStripMenuItem, faturalarToolStripMenuItem, yardımToolStripMenuItem, ayarlarToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1289, 86);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // anasayfaToolStripMenuItem
+            // 
+            anasayfaToolStripMenuItem.Image = Properties.Resources.anasayfaII;
+            anasayfaToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            anasayfaToolStripMenuItem.Name = "anasayfaToolStripMenuItem";
+            anasayfaToolStripMenuItem.Size = new Size(107, 82);
+            anasayfaToolStripMenuItem.Text = "Anasayfa";
+            anasayfaToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // ürünlerToolStripMenuItem
+            // 
+            ürünlerToolStripMenuItem.Image = Properties.Resources.product2;
+            ürünlerToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            ürünlerToolStripMenuItem.Name = "ürünlerToolStripMenuItem";
+            ürünlerToolStripMenuItem.Size = new Size(94, 82);
+            ürünlerToolStripMenuItem.Text = "Ürünler";
+            ürünlerToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // stoklarToolStripMenuItem
+            // 
+            stoklarToolStripMenuItem.Image = Properties.Resources.stok;
+            stoklarToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            stoklarToolStripMenuItem.Name = "stoklarToolStripMenuItem";
+            stoklarToolStripMenuItem.Size = new Size(88, 82);
+            stoklarToolStripMenuItem.Text = "Stoklar";
+            stoklarToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // müşterilerToolStripMenuItem
+            // 
+            müşterilerToolStripMenuItem.Image = Properties.Resources.musteriler;
+            müşterilerToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            müşterilerToolStripMenuItem.Name = "müşterilerToolStripMenuItem";
+            müşterilerToolStripMenuItem.Size = new Size(118, 82);
+            müşterilerToolStripMenuItem.Text = "Müşteriler";
+            müşterilerToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // şirketlerToolStripMenuItem
+            // 
+            şirketlerToolStripMenuItem.Image = Properties.Resources.company;
+            şirketlerToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            şirketlerToolStripMenuItem.Name = "şirketlerToolStripMenuItem";
+            şirketlerToolStripMenuItem.Size = new Size(101, 82);
+            şirketlerToolStripMenuItem.Text = "Şirketler";
+            şirketlerToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // personellerToolStripMenuItem
+            // 
+            personellerToolStripMenuItem.Image = Properties.Resources.personeller;
+            personellerToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            personellerToolStripMenuItem.Name = "personellerToolStripMenuItem";
+            personellerToolStripMenuItem.Size = new Size(127, 82);
+            personellerToolStripMenuItem.Text = "Personeller";
+            personellerToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // giderlerToolStripMenuItem
+            // 
+            giderlerToolStripMenuItem.Image = Properties.Resources.gelir;
+            giderlerToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            giderlerToolStripMenuItem.Name = "giderlerToolStripMenuItem";
+            giderlerToolStripMenuItem.Size = new Size(98, 82);
+            giderlerToolStripMenuItem.Text = "Giderler";
+            giderlerToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // kasaToolStripMenuItem
+            // 
+            kasaToolStripMenuItem.Image = Properties.Resources.safe;
+            kasaToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            kasaToolStripMenuItem.Name = "kasaToolStripMenuItem";
+            kasaToolStripMenuItem.Size = new Size(67, 82);
+            kasaToolStripMenuItem.Text = "Kasa";
+            kasaToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // bankalarToolStripMenuItem
+            // 
+            bankalarToolStripMenuItem.Image = Properties.Resources.bank;
+            bankalarToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            bankalarToolStripMenuItem.Name = "bankalarToolStripMenuItem";
+            bankalarToolStripMenuItem.Size = new Size(103, 82);
+            bankalarToolStripMenuItem.Text = "Bankalar";
+            bankalarToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // faturalarToolStripMenuItem
+            // 
+            faturalarToolStripMenuItem.Image = Properties.Resources.fatura;
+            faturalarToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            faturalarToolStripMenuItem.Name = "faturalarToolStripMenuItem";
+            faturalarToolStripMenuItem.Size = new Size(103, 82);
+            faturalarToolStripMenuItem.Text = "Faturalar";
+            faturalarToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // yardımToolStripMenuItem
+            // 
+            yardımToolStripMenuItem.Image = Properties.Resources.yardım;
+            yardımToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            yardımToolStripMenuItem.Name = "yardımToolStripMenuItem";
+            yardımToolStripMenuItem.Size = new Size(88, 82);
+            yardımToolStripMenuItem.Text = "Yardım";
+            yardımToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // ayarlarToolStripMenuItem
+            // 
+            ayarlarToolStripMenuItem.Image = Properties.Resources.settings;
+            ayarlarToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            ayarlarToolStripMenuItem.Name = "ayarlarToolStripMenuItem";
+            ayarlarToolStripMenuItem.Size = new Size(88, 82);
+            ayarlarToolStripMenuItem.Text = "Ayarlar";
+            ayarlarToolStripMenuItem.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1289, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem anasayfaToolStripMenuItem;
+        private ToolStripMenuItem ürünlerToolStripMenuItem;
+        private ToolStripMenuItem stoklarToolStripMenuItem;
+        private ToolStripMenuItem müşterilerToolStripMenuItem;
+        private ToolStripMenuItem şirketlerToolStripMenuItem;
+        private ToolStripMenuItem personellerToolStripMenuItem;
+        private ToolStripMenuItem giderlerToolStripMenuItem;
+        private ToolStripMenuItem kasaToolStripMenuItem;
+        private ToolStripMenuItem bankalarToolStripMenuItem;
+        private ToolStripMenuItem faturalarToolStripMenuItem;
+        private ToolStripMenuItem yardımToolStripMenuItem;
+        private ToolStripMenuItem ayarlarToolStripMenuItem;
+    }
+}
