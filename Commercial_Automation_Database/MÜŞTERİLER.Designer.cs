fe6aac38870a3d7691edbaf8f702d2c0879b0445
Furkan Kılıç <furkan.samet888@gmail.com>
2025-07-17 09:35:30 +0300

    Customers form was designed.

---
+++ Commercial_Automation_Database/MÜŞTERİLER.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace Commercial_Automation_Database
+{
+    partial class MÜŞTERİLER
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label7 = new Label();
+            TXTALIS = new TextBox();
+            label8 = new Label();
+            NUADET = new NumericUpDown();
+            label6 = new Label();
+            label5 = new Label();
+            MTXTYIL = new MaskedTextBox();
+            label4 = new Label();
+            TXTMARKA = new TextBox();
+            label3 = new Label();
+            TXTAD = new TextBox();
+            label2 = new Label();
+            TXTID = new TextBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            maskedTextBox1 = new MaskedTextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            richTextBox1 = new RichTextBox();
+            label9 = new Label();
+            textBox1 = new TextBox();
+            label10 = new Label();
+            ((System.ComponentModel.ISupportInitialize)NUADET).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(159, 471);
+            button3.Name = "button3";
+            button3.Size = new Size(185, 29);
+            button3.TabIndex = 18;
+            button3.Text = "GÜNCELLE";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(159, 521);
+            button2.Name = "button2";
+            button2.Size = new Size(185, 29);
+            button2.TabIndex = 17;
+            button2.Text = "SİL";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(159, 424);
+            button1.Name = "button1";
+            button1.Size = new Size(185, 29);
+            button1.TabIndex = 16;
+            button1.Text = "KAYDET";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label7.Location = new Point(102, 254);
+            label7.Name = "label7";
+            label7.Size = new Size(51, 28);
+            label7.TabIndex = 14;
+            label7.Text = "İLÇE:";
+            // 
+            // TXTALIS
+            // 
+            TXTALIS.Location = new Point(159, 187);
+            TXTALIS.Name = "TXTALIS";
+            TXTALIS.Size = new Size(185, 27);
+            TXTALIS.TabIndex = 13;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label8.Location = new Point(123, 220);
+            label8.Name = "label8";
+            label8.Size = new Size(30, 28);
+            label8.TabIndex = 12;
+            label8.Text = "İL:";
+            // 
+            // NUADET
+            // 
+            NUADET.Location = new Point(159, 274);
+            NUADET.Name = "NUADET";
+            NUADET.Size = new Size(0, 27);
+            NUADET.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label6.Location = new Point(92, 187);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 28);
+            label6.TabIndex = 10;
+            label6.Text = "MAIL:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label5.Location = new Point(116, 154);
+            label5.Name = "label5";
+            label5.Size = new Size(37, 28);
+            label5.TabIndex = 9;
+            label5.Text = "TC:";
+            // 
+            // MTXTYIL
+            // 
+            MTXTYIL.Location = new Point(159, 121);
+            MTXTYIL.Mask = "(999) 000-0000";
+            MTXTYIL.Name = "MTXTYIL";
+            MTXTYIL.Size = new Size(185, 27);
+            MTXTYIL.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label4.Location = new Point(58, 121);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 28);
+            label4.TabIndex = 6;
+            label4.Text = "TELEFON:";
+            // 
+            // TXTMARKA
+            // 
+            TXTMARKA.Location = new Point(159, 88);
+            TXTMARKA.Name = "TXTMARKA";
+            TXTMARKA.Size = new Size(185, 27);
+            TXTMARKA.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label3.Location = new Point(75, 88);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 28);
+            label3.TabIndex = 4;
+            label3.Text = "SOYAD:";
+            // 
+            // TXTAD
+            // 
+            TXTAD.Location = new Point(159, 55);
+            TXTAD.Name = "TXTAD";
+            TXTAD.Size = new Size(185, 27);
+            TXTAD.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label2.Location = new Point(110, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 28);
+            label2.TabIndex = 2;
+            label2.Text = "AD:";
+            // 
+            // TXTID
+            // 
+            TXTID.Location = new Point(159, 22);
+            TXTID.Name = "TXTID";
+            TXTID.Size = new Size(185, 27);
+            TXTID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label1.Location = new Point(118, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 28);
+            label1.TabIndex = 0;
+            label1.Text = "ID:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(richTextBox1);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(TXTALIS);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(NUADET);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(MTXTYIL);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(TXTMARKA);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(TXTAD);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(TXTID);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(741, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(359, 574);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "MÜŞTERİ BİLGİLERİ";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(694, 574);
+            dataGridView1.TabIndex = 2;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(159, 154);
+            maskedTextBox1.Mask = "00000000000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(185, 27);
+            maskedTextBox1.TabIndex = 19;
+            maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(159, 220);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(185, 28);
+            comboBox1.TabIndex = 20;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(159, 254);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(185, 28);
+            comboBox2.TabIndex = 21;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(159, 295);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(185, 71);
+            richTextBox1.TabIndex = 22;
+            richTextBox1.Text = "";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label9.Location = new Point(77, 310);
+            label9.Name = "label9";
+            label9.Size = new Size(76, 28);
+            label9.TabIndex = 23;
+            label9.Text = "ADRES:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(159, 375);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(185, 27);
+            textBox1.TabIndex = 24;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label10.Location = new Point(49, 371);
+            label10.Name = "label10";
+            label10.Size = new Size(104, 28);
+            label10.TabIndex = 25;
+            label10.Text = "VERGİ NO:";
+            // 
+            // MÜŞTERİLER
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1112, 615);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            Name = "MÜŞTERİLER";
+            Text = "MÜŞTERİLER";
+            ((System.ComponentModel.ISupportInitialize)NUADET).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label7;
+        private TextBox TXTALIS;
+        private Label label8;
+        private NumericUpDown NUADET;
+        private Label label6;
+        private Label label5;
+        private MaskedTextBox MTXTYIL;
+        private Label label4;
+        private TextBox TXTMARKA;
+        private Label label3;
+        private TextBox TXTAD;
+        private Label label2;
+        private TextBox TXTID;
+        private Label label1;
+        private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private MaskedTextBox maskedTextBox1;
+        private RichTextBox richTextBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label10;
+        private TextBox textBox1;
+        private Label label9;
+    }
+}
